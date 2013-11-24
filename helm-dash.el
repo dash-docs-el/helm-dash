82849bc07523742b9cb3933e03a7f0ee4916b202
Raimon Grau <raimonster@gmail.com>
2013-11-24 18:58:56 +0100

    Trying it with Go

--- helm-dash.el
+++ helm-dash.el
@@ -2,14 +2,14 @@
 (require 'sqlite)
 
 (defun helm-dash-search ()
- (let ((conn (sqlite-init "Vim.docset/Contents/Resources/docSet.dsidx"))
+ (let ((conn (sqlite-init "Go.docset/Contents/Resources/docSet.dsidx"))
        (db "searchIndex")
        (res nil))
-   (setq res (sqlite-query conn (format "SELECT * FROM %s WHERE \"name\" like \"%%s%\" where " db helm-pattern)))
+   (setq res (sqlite-query conn (format "SELECT type, name, path FROM %s WHERE \"name\" like \"%%%s%%\"" db helm-pattern)))
    (sqlite-bye conn)
-
-   (message res)
-   ))
+   (mapcar (lambda (x)
+                   (cons (cadr x) (caddr x)))
+           res)))
 
 (defvar helm-source-dash-search
   '((name . "Dash")
