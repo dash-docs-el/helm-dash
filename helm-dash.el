32853dea39b04044892beacb33dfb3b5878507a1
Raimon Grau <raimonster@gmail.com>
2013-11-24 19:19:54 +0100

    Basic functionality works

--- helm-dash.el
+++ helm-dash.el
@@ -8,15 +8,24 @@
    (setq res (sqlite-query conn (format "SELECT type, name, path FROM %s WHERE \"name\" like \"%%%s%%\"" db helm-pattern)))
    (sqlite-bye conn)
    (mapcar (lambda (x)
-                   (cons (cadr x) (caddr x)))
+                   (cons (cadr x) (format "%s%s%s"
+                                          default-directory
+                                          "Go.docset/Contents/Resources/Documents/"
+                                          (caddr x))))
            res)))
+
+;; (defun helm-dash-browse-webdoc (webdoc)
+;;   (browse-url-chromium  (format "%s/Go.docset/Contents/Resources/Documents/godoc.org/code.google.com/p/go.crypto/ssh.html#VDSUSP" default-directory)))
+
+(defun helm-dash-actions (actions doc-item) `(("Go to doc" . browse-url))
+  )
 
 (defvar helm-source-dash-search
   '((name . "Dash")
     (volatile)
     (delayed)
     (multiline)
-    (requires-pattern . 2)
+    (requires-pattern . 3)
     (candidates-process . helm-dash-search)
     (action-transformer . helm-dash-actions)))
 
