e23c26ed8559b3bc5202829c54402556c283fa00
Toni Reina <areina0@gmail.com>
2014-10-20 16:24:37 +0200

    Add URL info in headers.

--- helm-dash.el
+++ helm-dash.el
@@ -4,6 +4,8 @@
 
 ;; Author: Raimon Grau <raimonster@gmail.com>
 ;;         Toni Reina  <areina0@gmail.com>
+;;
+;; URL: http://github.com/areina/helm-dash
 ;; Version: 0.1
 ;; Package-Requires: ((helm "0.0.0"))
 ;; Keywords: docs
